baa320a73c4483b8839711589d364310b36ec8f0
Ashar <asharsaleem55@gmail.com>
2023-05-01 22:07:02 +0500

    Running, changes some files code using chatGpt

--- AuxProcessing.py
+++ AuxProcessing.py
@@ -1,51 +1,40 @@
 #!/usr/bin/env python3
 
-# Package imports
+# Import required packages
 from dotenv import load_dotenv
 import binascii
 import os
 
-load_dotenv()  # take environment variables from .env
+load_dotenv()  # Load environment variables from .env file
 
 
 class AuxProcessing:
 
     @staticmethod
     def IntegersToBinary(integer_representation) -> str:
-        '''This assumes that each integer in the integer representation should be representated as 4 bits, that is, '0' in dec is '0000' in binary, even though '0' can also be used. This is done so for ease of parsing here'''
+        '''Converts integer representation to binary representation using a 4-bit format per integer'''
         return str(''.join([((int(os.environ['ENTRY_LENGTH']) - len(bin_value)) * '0') + bin_value for bin_value in [bin(int(character))[2:] for character in str(integer_representation)]]))
 
     @staticmethod
     def BinaryToIntegers(binary_representation) -> int:
-        '''This assumes that each representation is
-        split into a nibble format, that is, the number
-        of bits to represent a single value consists of 4 bits, regardless of the value, from 0000 to 1111'''
+        '''Converts binary representation to integer representation using a nibble format'''
         return int(''.join([str(int(binary_representation[index:index+4], 2)) for index in range(0, len(binary_representation), 4)]))
 
     @staticmethod
     def UTF8ToBinary(utf8_representation, encoding='utf-8', errors='surrogatepass') -> str:
-        '''According to the UTF-8 standard,
-
-        Unciode Range           # UTF-8 Bytes           Characters
-        \u000000-\u00007F       1 byte                  The first 128 characters (US-ASCII)
-        \u000080-\u0007FF       2 bytes                 Latin characters with diacritics and Greek, etc
-        \u000800-\u00FFFF       3 bytes                 Rest of the basic multilingual plane
-        \u010000-\u10FFFF       4 bytes                 All the other unicode characters
-
-        Thus, for now, I'm going to represent a character only as 1 byte because of the files that I have
-        can be easily be represented as ASCII. Which means each character will be of 8 bits exactly, from
-        0-127 in the ASCII representation
-        '''
+        '''Converts UTF-8 representation to binary representation using 8-bit format'''
         bits = bin(int(binascii.hexlify(
             utf8_representation.encode(encoding, errors)), 16))[2:]
         return bits.zfill(8 * ((len(bits) + 7) // 8))
 
     @staticmethod
     def BinaryToUTF8(binary_representation, encoding='utf-8', errors='surrogatepass') -> str:
+        '''Converts binary representation to UTF-8 representation'''
         return AuxProcessing.IntegerToBytes(int(binary_representation, 2)).decode(encoding, errors)
 
     @staticmethod
     def IntegerToBytes(integer_representation) -> bytes:
+        '''Converts integer representation to byte representation'''
         hex_string = '%x' % integer_representation
         number = len(hex_string)
         return binascii.unhexlify(hex_string.zfill(number + (number & 1)))
--- TCPControlFlags.py
+++ TCPControlFlags.py
@@ -1,22 +1,21 @@
 class TCPControlFlags():
-
     def __init__(self):
         '''Control Bits govern the entire process of connection establishment, data transmissions and connection termination. The control bits are listed as follows, they are'''
 
         '''It indicates if we need to use Urgent pointer field or not. If it is set to 1 then only we use Urgent pointer.'''
-        self.URG = 0x0
+        self.URG = False
 
         '''It is set when an acknowledgement is being sent to the sender.'''
-        self.ACK = 0x0
+        self.ACK = False
 
         '''When the bit is set, it tells the receiving TCP module to pass the data to the application immediately.'''
-        self.PSH = 0x0
+        self.PSH = False
 
         '''When the bit is set, it aborts the connection. It is also used as a negative acknowledgement against a connection request.'''
-        self.RST = 0x0
+        self.RST = False
 
         '''It is used during the initial establishment of a connection. It is set when synchronizing process is initiated.'''
-        self.SYN = 0x0
+        self.SYN = False
 
         '''The bit indicates that the host that sent the FIN bit has no more data to send.'''
-        self.FIN = 0x0
+        self.FIN = False
